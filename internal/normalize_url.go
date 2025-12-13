d524801c22039b072bb269cf9c6ac74bc90a91aa
richard <m.richardp93@gmail.com>
2025-12-13 10:11:46 -0500

    wrote normalize function and got to where test cases passed

--- internal/normalize_url.go
+++ internal/normalize_url.go
@@ -1,5 +1,26 @@
 package internal
 
+import (
+	"log"
+	"net/url"
+	"strings"
+)
+
 func NormalizeURL(inputURL string) (string, error) {
-	return "", nil
+	u, err := url.Parse(inputURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Printf("scheme: %v\n\n", u.Scheme)
+	// fmt.Printf("user:%v\n\n", u.User)
+	// fmt.Printf("host: %v\n\n", u.Host)
+	// fmt.Printf("path: %v\n\n", u.Path)
+	// fmt.Printf("rawpath: %v\n\n", u.RawPath)
+
+	cleanHost := strings.TrimSuffix(strings.ToLower(u.Host), "/")
+	cleanPath := strings.TrimSuffix(u.Path, "/")
+
+	cleanURL := cleanHost + cleanPath
+	return cleanURL, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/datsun80zx/webscrpr.git/internal"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	url := "https://www.boot.dev/lessons/98ac1f38-22dd-4682-b114-8638a0625567"
+
+	parsedURL, err := internal.NormalizeURL(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("func returns:", parsedURL)
 }
